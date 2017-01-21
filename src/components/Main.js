623afc44653e7a3d8721699dbb3a839f36d66919
Blake Watkins <iamsoblake@gmail.com>
2017-01-20 20:50:48 -0800

    added react bootstrap module

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,36 +1,52 @@
 // ./src/components/Main.js
 import React, {Component} from 'react';
+import {Navbar, NavItem, MenuItem, Nav, NavDropdown} from 'react-bootstrap';
 import { Link } from 'react-router';
 import './../styles/Main.css'
 
 class Main extends Component {
-    render(){
-        return(
+  render(){
+    return (
             <div>
-                <nav className="navbar navbar-default">
-                    <div className="container-fluid">
-                        <div className="navbar-header">
-                            <Link className="navbar-brand" to="/">RaicesPeruanas</Link>
-                        </div>
-                        <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                            <ul className="nav navbar-nav">
-                                <li><Link to="/">Home</Link></li>
-                                <li><Link to="/history">History</Link></li>
-                                <li><Link to="/about">About</Link></li>
-                                <li><Link to="/dances">Dances</Link></li>
-                                <li><Link to="/photos">Photos</Link></li>
-                                <li><Link to="/news">News</Link></li>
-                                <li><Link to="/dancers">Dancers</Link></li>
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
-                <div className="container-fluid">
+  <Navbar inverse collapseOnSelect>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <Link to="/">RaicesPeruanas</Link>
+      </Navbar.Brand>
+      <Navbar.Toggle />
+    </Navbar.Header>
+    <Navbar.Collapse>
+      <Nav>
+        <NavItem eventKey={1} href="#"><Link to="/">Home</Link></NavItem>
+        <NavItem eventKey={2} href="#"><Link to="/history">History</Link></NavItem>
+        <NavItem eventKey={3} href="#"><Link to="/about">About</Link></NavItem>
+        <NavItem eventKey={4} href="#"><Link to="/dances">Dances</Link></NavItem>
+        <NavItem eventKey={5} href="#"><Link to="/photos">Photos</Link></NavItem>
+        <NavItem eventKey={6} href="#"><Link to="/news">News</Link></NavItem>
+        <NavItem eventKey={7} href="#"><Link to="/dancers">Dancers</Link></NavItem>
+      </Nav>
+      <Nav pullRight>
+        <NavItem eventKey={1} href="#">Link Right</NavItem>
+        <NavDropdown eventKey={2} title="Dropdown" id="basic-nav-dropdown">
+          <MenuItem eventKey={2.1}><Link to="/">Home</Link></MenuItem>
+          <MenuItem eventKey={2.2}><Link to="/history">History</Link></MenuItem>
+          <MenuItem eventKey={2.3}><Link to="/about">About</Link></MenuItem>
+          <MenuItem eventKey={2.4}><Link to="/dances">Dances</Link></MenuItem>
+          <MenuItem eventKey={2.5}><Link to="/photos">Photos</Link></MenuItem>
+          <MenuItem eventKey={2.6}><Link to="/news">News</Link></MenuItem>
+          <MenuItem eventKey={2.7}><Link to="/dancers">Dancers</Link></MenuItem>
+          <MenuItem divider />
+          <MenuItem eventKey={2.8}>Separated link</MenuItem>
+        </NavDropdown>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+    <div className="container-fluid">
                     {this.props.children}
                 </div>
             </div>
-        );
-    }
+)
+}
 }
 
 export default Main;
---
+++ src/components/scratchComponents.js
@@ -0,0 +1,30 @@
+
+class Main extends Component {
+    render(){
+        return(
+            <div>
+                <nav className="navbar navbar-default">
+                    <div className="container-fluid">
+                        <div className="navbar-header">
+                            <Link className="navbar-brand" to="/">RaicesPeruanas</Link>
+                        </div>
+                        <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                            <ul className="nav navbar-nav">
+                                <li><Link to="/">Home</Link></li>
+                                <li><Link to="/history">History</Link></li>
+                                <li><Link to="/about">About</Link></li>
+                                <li><Link to="/dances">Dances</Link></li>
+                                <li><Link to="/photos">Photos</Link></li>
+                                <li><Link to="/news">News</Link></li>
+                                <li><Link to="/dancers">Dancers</Link></li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+                <div className="container-fluid">
+                    {this.props.children}
+                </div>
+            </div>
+        );
+    }
+}
