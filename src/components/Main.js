e3a1396489fc9e0b04c227dcf8d0168945b0665c
Blake Watkins <iamsoblake@gmail.com>
2017-01-21 20:38:18 -0800

    adjusting styles

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,6 +3,7 @@ import React, {
   Component
 } from 'react';
 import {
+  Image,
   Navbar,
   NavItem,
   MenuItem,
@@ -10,10 +11,11 @@ import {
   NavDropdown
 } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
-import {
-  Link
-} from 'react-router';
-import './../styles/Main.css'
+// import {
+  // Link
+// } from 'react-router';
+import './../styles/Main.css';
+import logo from './../assets/image1-small-23.png';
 
 class Main extends Component {
   render() {
@@ -21,32 +23,44 @@ class Main extends Component {
       <section>
         <Navbar collapseOnSelect fluid fixedTop>
           <Navbar.Header>
-            <Navbar.Brand><Link to="/">RaicesPeruanas</Link></Navbar.Brand>
+            {/*<Navbar.Brand>*/}
+              <Nav pullLeft className="infos">
+                <NavItem eventKey={1} href="#"><Image src={logo} height="25" width="25" responsive circle/></NavItem>
+                <NavItem eventKey={2} href="#"><i className="fa fa-phone" aria-hidden="true"></i> Call 1(333)333-3333</NavItem>
+                <NavItem eventKey={3} href="#"><i className="fa fa-envelope-o" aria-hidden="true"></i> Mail: mail@example.com</NavItem>
+              </Nav>
+              <Nav pullRight className="socials">
+                <NavItem eventKey={1} className="fa fa-twitter fa-2x" href="#"></NavItem>
+                <NavItem eventKey={2} className="fa fa-youtube fa-2x" href="#"></NavItem>
+                <NavItem eventKey={3} className="fa fa-facebook fa-2x" href="#"></NavItem>
+              </Nav>
+              {/*</Navbar.Brand>*/}
             <Navbar.Toggle />
           </Navbar.Header>
           <Navbar.Collapse>
-            <Nav pullLeft>
-              <LinkContainer to="/"><NavItem eventKey={1} href="#">Home</NavItem></LinkContainer>
-              <LinkContainer to="history"><NavItem eventKey={2} href="#">History</NavItem></LinkContainer>
-              <LinkContainer to="about"><NavItem eventKey={3} href="#">About</NavItem></LinkContainer>
-              <LinkContainer to="dances"><NavItem eventKey={4} href="#">Dances</NavItem></LinkContainer>
-              <LinkContainer to="photos"><NavItem eventKey={5} href="#">Photos</NavItem></LinkContainer>
-              <LinkContainer to="news"><NavItem eventKey={6} href="#">News</NavItem></LinkContainer>
-              <LinkContainer to="dancers"><NavItem eventKey={7} href="#">Dancers</NavItem></LinkContainer>
+            <Nav pullLeft disabled>
+              <LinkContainer to="/"><NavItem eventKey={1} href="#">RaicesPeruanas</NavItem></LinkContainer>
             </Nav>
             <Nav pullRight>
-              <NavItem eventKey={1} href="#">Contact</NavItem>
-              <NavDropdown eventKey={2} title="Menu" id="basic-nav-dropdown">
-                <LinkContainer to="/"><MenuItem eventKey={2.1}>Home</MenuItem></LinkContainer>
-                <LinkContainer to="history"><MenuItem eventKey={2.2}>History</MenuItem></LinkContainer>
-                <LinkContainer to="about"><MenuItem eventKey={2.3}>About</MenuItem></LinkContainer>
+              {/*<LinkContainer to="/"><NavItem eventKey={1} href="#">Home</NavItem></LinkContainer>*/}
+              <LinkContainer to="history"><NavItem eventKey={1} href="#">History</NavItem></LinkContainer>
+              <LinkContainer to="about"><NavItem eventKey={2} href="#">About</NavItem></LinkContainer>
+              <LinkContainer to="dances"><NavItem eventKey={3} href="#">Dances</NavItem></LinkContainer>
+              <LinkContainer to="photos"><NavItem eventKey={4} href="#">Photos</NavItem></LinkContainer>
+              <LinkContainer to="news"><NavItem eventKey={5} href="#">News</NavItem></LinkContainer>
+              <LinkContainer to="dancers"><NavItem eventKey={6} href="#">Dancers</NavItem></LinkContainer>
+              {/*<LinkContainer to="contact"><NavItem eventKey={7} href="#">Contact</NavItem></LinkContainer>*/}
+              <NavDropdown eventKey={7} title="Menu" id="basic-nav-dropdown">
+                <LinkContainer to="/"><MenuItem eventKey={7.1}>Home</MenuItem></LinkContainer>
+                <LinkContainer to="history"><MenuItem eventKey={7.2}>History</MenuItem></LinkContainer>
+                <LinkContainer to="about"><MenuItem eventKey={7.3}>About</MenuItem></LinkContainer>
 
-                <LinkContainer to="dances"><MenuItem eventKey={2.4}>Dances</MenuItem></LinkContainer>
-                <LinkContainer to="photos"><MenuItem eventKey={2.5}>Photos</MenuItem></LinkContainer>
-                <LinkContainer to="news"><MenuItem eventKey={2.6}>News</MenuItem></LinkContainer>
-                <LinkContainer to="dancers"><MenuItem eventKey={2.7}>Dancers</MenuItem></LinkContainer>
+                <LinkContainer to="dances"><MenuItem eventKey={7.4}>Dances</MenuItem></LinkContainer>
+                <LinkContainer to="photos"><MenuItem eventKey={7.5}>Photos</MenuItem></LinkContainer>
+                <LinkContainer to="news"><MenuItem eventKey={7.6}>News</MenuItem></LinkContainer>
+                <LinkContainer to="dancers"><MenuItem eventKey={7.7}>Dancers</MenuItem></LinkContainer>
                 <MenuItem divider />
-                <MenuItem eventKey={2.8}>Contact</MenuItem>
+                <MenuItem eventKey={7.8}>Contact</MenuItem>
               </NavDropdown>
             </Nav>
           </Navbar.Collapse>
