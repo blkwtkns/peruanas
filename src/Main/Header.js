f820481b9d286a1bf209c7257bf0ea8a480e3241
Blake Watkins <iamsoblake@gmail.com>
2017-02-05 14:19:55 -0800

    changed home link to logo and fixed some css

--- src/Main/Header.js
+++ src/Main/Header.js
@@ -18,31 +18,26 @@ class Header extends Component {
 
     return (
       <Navbar fluid fixedTop collapseOnSelect className="navMain">
-        {/* <Navbar.Header>
-          <Navbar.Brand>
-            <LinkContainer to="/">
-              <a href="#">
-                <Image className="raicesLogo" src={ logo } circle/> 
-                <span className='title'>RaicesPeruanas</span>
-              </a>
-            </LinkContainer>
-          </Navbar.Brand>
-          <Navbar.Toggle />
-        </Navbar.Header> */}
+
         <NavBrand />
+
         <Navbar.Collapse>
           <Nav pullRight className="links">
+            <div className="routeLinks">
             <LinkContainer to="news"><NavItem eventKey={2} href="#">News</NavItem></LinkContainer>
             <LinkContainer to="dances"><NavItem eventKey={3} href="#">Dances</NavItem></LinkContainer>
             <LinkContainer to="photos"><NavItem eventKey={4} href="#">Photos</NavItem></LinkContainer>
             <LinkContainer to="history"><NavItem eventKey={5} href="#">History</NavItem></LinkContainer>
             <LinkContainer to="dancers"><NavItem eventKey={6} href="#">Dancers</NavItem></LinkContainer>
             <LinkContainer to="contact"><NavItem eventKey={7} href="#">Contact</NavItem></LinkContainer>
+          </div>
+          <div className="socialLinks">
             <a className="fa fa-phone fa-2x socials" href="tel:6266640944"></a>
             <a className="fa fa-envelope-o fa-2x socials" href="mailto:raicesperuanas@hotmail.com"></a>
             <a className="fa fa-twitter fa-2x socials" href="#"></a>
             <a className="fa fa-youtube fa-2x socials" href="http://www.youtube.com/user/RaicesPeruanas/videos"></a>
             <a className="fa fa-facebook fa-2x socials" href="https://www.facebook.com/danzas.peruanas"></a>
+          </div>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
