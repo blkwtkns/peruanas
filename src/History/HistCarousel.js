7cac9d5844f67f77086501784be6bc5c1a7e52b7
Blake Watkins <iamsoblake@gmail.com>
2017-02-05 19:59:23 -0800

    adding loading of pics to components

--- src/History/HistCarousel.js
+++ src/History/HistCarousel.js
@@ -1,22 +1,45 @@
 import React from 'react';
 import { Carousel, Image } from 'react-bootstrap';
+/* const history = require('../constants/photoState').history; */
+import { history } from '../constants/photoState';
 
-const gazers = require('./../assets/dancers-gaze.jpg');
-const bow = require('./../assets/foto8.jpg');
-const bigSmile = require('./../assets/foto21.jpg');
+/* const gazers = require('./../assets/history/girl_scanned_01s.jpg');  */
+/* const bow = require('./../assets/history/girls_scanned_01s.jpg');  */
+/* const bigSmile = require('./../assets/history/girls_scanned_02s.jpg');  */
 
-
+// you idiot use componentDidMount lol
 const HistCarousel = (props) => {
-  
-  const HistoryItems = [gazers, bow, bigSmile].map((pic, i) => {
-    return <Carousel.Item key={i}><Image src={pic} key={i} responsive/></Carousel.Item>
-  })
 
-  return (
-  <Carousel className="HistCarousel">
-    {HistoryItems} 
-  </Carousel>
-  );
+  return (() => {
+    const [a,b,c,d,e,f,g,h,i,j,k,l,m] = require('../constants/photoState').history;
+    return Promise.resolve([a,b,c,d,e,f,g,h,i,j,k,l,m]);
+  })()
+    .then( items => {
+      return items.map((pic, i) => {
+        return <Carousel.Item key={i}><Image src={pic} key={i} responsive/></Carousel.Item>
+      })
+    })
+
+    .then( mapped => {
+      console.log('mapped', mapped)
+
+      return( <Carousel className="HistCarousel">
+        {mapped}
+      </Carousel>
+      )
+
+    })
+
+
+  /* const HistoryItems = [gazers, bow, bigSmile].map((pic, i) => {
+   *   return <Carousel.Item key={i}><Image src={pic} key={i} responsive/></Carousel.Item>
+   * }) */
+
+  /* return (
+   *   <Carousel className="HistCarousel">
+   *     { createFragment(HistoryItems) } 
+   *   </Carousel>
+   * ); */
 }
 
 export default HistCarousel;
--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,37 +1,32 @@
 // ./src/Main/Main.js
 import React, {Component} from 'react';
-import {
-  connect
-} from 'react-redux';
+/* import {
+ *   connect
+ * } from 'react-redux'; */
 import Footer from './Footer';
 import Header from './Header'
 
-/* if (process.env.WEBPACK) require('./main.css'); */
-/* require('./main.scss'); */
-/* import styles from './main.css'; */
+const Main = (props) => {
+
+  /* const childrenWithProps = React.Children.map(this.props.children,
+   *       (child) => React.cloneElement(child, {pics: this.props.pics})
+   *     ); */
+
+  return (
+    <section>
+      <Header />
+      <div className="container-fluid">
+        { props.children }
+      </div>
+      <Footer />
+    </section>
+  );
+} 
 
 
-class Main extends Component {
+/* export default connect( ({state}) => ({
+ *     pics: state.shared.pics
+ *   })
+ *   )(Main); */
 
-  render(){
-    /* const childrenWithProps = React.Children.map(this.props.children,
-     *       (child) => React.cloneElement(child, {pics: this.props.pics})
-     *     ); */
-
-    return (
-      <section>
-        <Header />
-        <div className="container-fluid">
-          { this.props.children }
-        </div>
-        <Footer />
-      </section>
-    );
-  } 
-}
-/* export default Main; */
-
-export default connect( ({state}) => ({
-    pics: state.shared.pics
-  })
-  )(Main);
+export default Main;
